e2f1e7253dc358c389822bc5837b8482c1ee4f42
Jon Cinque <jon.cinque@gmail.com>
2022-12-03 19:50:38 +0100

    Add test

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,21 @@ const FROM_EMAIL: &str = "Secret Santa";
 type Email<'a> = &'a str;
 type Name<'a> = &'a str;
 
-fn assign_santas<'a>(mut participants: Vec<(Name<'a>, Email<'a>)>) -> HashMap<(Name, Email), Name> {
-    let mut santas = HashMap::new();
+fn assign_santas<'a>(mut participants: Vec<(Name<'a>, Email<'a>)>) -> HashMap<Name, (Email, Name)> {
+    let mut santas = HashMap::<Name, (Email, Name)>::new();
     let mut unassigned_santas = participants.clone();
 
     let mut rng = rand::thread_rng();
     participants.shuffle(&mut rng);
     for participant in participants.iter() {
         let mut santa = *unassigned_santas.choose(&mut rng).unwrap();
-        while santa.0 == participant.0 || santas.get(participant) == Some(&santa.0) {
+        while santa.0 == participant.0 || santas.get(participant.0).map(|s| s.1) == Some(&santa.0) {
             santa = *unassigned_santas.choose(&mut rng).unwrap();
         }
 
         unassigned_santas.retain(|&s| s.0 != santa.0);
         println!("{} gives a gift to {}", santa.0, participant.0);
-        santas.insert(santa, participant.0);
+        santas.insert(santa.0, (santa.1, participant.0));
     }
     santas
 }
@@ -75,15 +75,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .collect();
     let santas = assign_santas(participants);
 
-    for (santa, recipient) in santas {
+    for (santa, (email, recipient)) in santas {
         let email = Message::builder()
             .from(from.parse()?)
             .reply_to(reply_to.parse()?)
-            .to(santa.1.parse()?)
+            .to(email.parse()?)
             .subject("Secret Santa")
             .body(format!(
                 "Hey {}, it's Secret Santa.  Get a gift for {}!",
-                santa.0, recipient
+                santa, recipient
             ))?;
         // types are gross, we can probably do this better some other time
         if matches.get_flag("test") {
@@ -97,4 +97,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
         };
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::assign_santas;
+
+    #[test]
+    fn success_assign() {
+        let participants = vec![
+            ("1", ""),
+            ("2", ""),
+            ("3", ""),
+            ("4", ""),
+            ("5", ""),
+            ("6", ""),
+            ("7", ""),
+            ("8", ""),
+        ];
+        let santas = assign_santas(participants);
+        for (santa, (_, recipient)) in &santas {
+            let recipient_of_recipient = &santas.get(recipient).unwrap().1;
+            assert_ne!(santa, recipient_of_recipient);
+        }
+    }
 }
